e41202f874c8def212349887c038d215bf3306f4
zygmunt7 <zygmunt.jasinski@pollub.edu.pl>
2022-04-23 10:39:44 +0100

    commit 2 Add Car Class

---
+++ src/vehicles/Car.java
@@ -0,0 +1,23 @@
+package vehicles;
+
+public class Car {
+    private String brand;
+    private String model;
+
+    public Car(String brand, String model) {
+        this.brand = brand;
+        this.model = model;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void show() {
+        System.out.println("Informacje o samochodzie: " + brand + " " + model);
+    }
+}
